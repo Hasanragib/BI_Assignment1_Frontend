19c7911da50146b6ba66c2f267695645086ef383
Ragib Hasan <hragib1@gmail.com>
2026-04-20 21:12:27 +0530

    Chores: updated the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // This tells Vite to use the modern Sass compiler API
+        api: "modern-compiler",
+      },
+    },
+  },
+});
