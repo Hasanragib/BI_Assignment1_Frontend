afe3a5d43e74fb7de37fba319723d6dfeb2a4682
Ragib Hasan <hragib1@gmail.com>
2026-04-20 21:23:54 +0530

    chores: Added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 import Events from "./components/Events.jsx";
 import EventsDetails from "./components/EventsDetails.jsx";
